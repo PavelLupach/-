f1ab91d8e1ae142f0c9cd967c24566cb98319dd7
PavelLupach <145809339+PavelLupach@users.noreply.github.com>
2023-09-23 14:17:43 +0300

    Add files via upload
    Стоят два стакана. 
    Один наполненный, второй пустой. 
    Наполненный говорит пустому:
    - Нет ну вылитый я.

---
+++ Калькулятор/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace УСРС2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fValue, sValue;
+            string action;
+
+            Console.WriteLine("Введите первое число: ");
+            fValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Выберите операцию: '+' '-' '*' '/' ");
+            action = Console.ReadLine();
+
+            Console.WriteLine("Введите второе число: ");
+            sValue = double.Parse(Console.ReadLine());
+
+            switch (action)
+            {
+               
+                
+                case "+":
+                    Console.WriteLine(fValue + sValue);
+                break;
+
+                case "-":
+                    Console.WriteLine(fValue - sValue);
+                break;
+
+                case "*":
+                    Console.WriteLine(fValue * sValue);
+                break;
+
+                case "/":
+
+                    if (sValue == 0)
+                {
+                    Console.WriteLine("НА 0 ДЕЛИТЬ НЕЛЬЗЯ!");
+                }
+                else
+                {
+                    Console.WriteLine(fValue / sValue);
+                }
+
+                Console.WriteLine(fValue / sValue);
+                break;
+
+                default:
+                    Console.WriteLine("Ошибка ввода");
+                break;
+            
+
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ УСРС1/УСРС1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace УСРС1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine($"1 a = {a}");
+            
+            a += 10;
+            Console.WriteLine($"2 a = {a}");
+
+            a -= 4;
+            Console.WriteLine($"3 a = {a}");
+
+            a *= 2;
+            Console.WriteLine($"4 a = {a}");
+
+            a %= 8;
+            Console.WriteLine($"5 a = {a}");
+
+            a |= 4;
+            Console.WriteLine($"6 a = {a}");
+
+            a &= 2;
+            Console.WriteLine($"7 a = {a}");
+
+            int b = 6;
+            Console.WriteLine($"b = {b}");
+
+            int c = a += b -= 5;
+            Console.WriteLine($"c = {c}");
+        }
+    }
+}
