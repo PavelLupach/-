0685fbce9b6598e47eb4414ef0415fb3ffbe2630
Anastasia <girl-physicist@mail.ru>
2023-09-26 17:28:38 +0300

    recomend.

--- Калькулятор/Program.cs
+++ Калькулятор/Program.cs
@@ -8,24 +8,20 @@ namespace УСРС2
 {
     class Program
     {
+            double fValue, sValue;// лучше firstValue
+            string action;
+            // в методе Main не должно быть расчетов, весь функционал выносится в отдельные методы, а тут только их вызывают
         static void Main(string[] args)
         {
-            double fValue, sValue;
-            string action;
+            GetInputData();
+            Calc();                     
+            Console.ReadLine();
+        }
 
-            Console.WriteLine("Введите первое число: ");
-            fValue = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Выберите операцию: '+' '-' '*' '/' ");
-            action = Console.ReadLine();
-
-            Console.WriteLine("Введите второе число: ");
-            sValue = double.Parse(Console.ReadLine());
-
+        static void Calc()
+        {
             switch (action)
-            {
-               
-                
+            {           
                 case "+":
                     Console.WriteLine(fValue + sValue);
                 break;
@@ -58,8 +54,20 @@ namespace УСРС2
             
 
             }
-            Console.ReadLine();
         }
+        static void GetInputData()
+        {
+             Console.WriteLine("Введите первое число: ");
+            fValue = double.Parse(Console.ReadLine()); // следовало добавить обработку исключения на случай, если не удалось конвертировать строку в число (например ввели дробное число или буквы)
+
+            Console.WriteLine("Выберите операцию: '+' '-' '*' '/' ");
+            action = Console.ReadLine();
+
+            Console.WriteLine("Введите второе число: ");
+            sValue = double.Parse(Console.ReadLine());
+
+        }
+            
 
     }
 }
