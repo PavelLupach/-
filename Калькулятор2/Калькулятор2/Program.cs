a207f7665d9a561496211e0fab43e63c9d45fb9b
PavelLupach <145809339+PavelLupach@users.noreply.github.com>
2023-10-01 22:47:19 +0300

    Add files via upload
    тот же калькулятор только на if else. По моему вышел лучше чем первый.

---
+++ Калькулятор2/Калькулятор2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Калькулятор2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstValue, secondValue;
+            string action;
+
+            Console.WriteLine("Введите превое число: ");
+            firstValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Выберите операцию: '+' '-' '*' '/'");
+            action = Console.ReadLine();
+            
+            Console.WriteLine("Введите второе число: ");
+            secondValue = double.Parse(Console.ReadLine());
+
+            if (action =="+")
+            {
+                Console.WriteLine(firstValue + secondValue);
+            }
+            else if (action == "-")
+            {
+                Console.WriteLine(firstValue - secondValue);
+            }
+            else if (action == "*")
+            {
+                Console.WriteLine(firstValue * secondValue);
+            }
+            else if (action == "/")
+            {
+                if (secondValue == 0)
+                    Console.WriteLine("НА НУЛЬ ДЕЛИТЬ НЕЛЬЗЯ!");
+                else
+                    Console.WriteLine(firstValue / secondValue);
+            }
+            else
+            {
+                Console.WriteLine("Я без понятия что с этим делать");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
